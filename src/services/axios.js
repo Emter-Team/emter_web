cdc3377ec4cb1cbe21895c9e90e24ebadab50d46
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2024-07-02 13:22:21 +0700

    chore: change url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: "https://845d-114-122-20-86.ngrok-free.app/",
     withCredentials: true,
 });
 
